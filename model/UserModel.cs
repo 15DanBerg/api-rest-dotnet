06d8a52b827fe9e0df8bb5bf3ce088e2dca6a1fc
Daniel Berg <daniel.berg031@gmail.com>
2025-08-20 10:46:01 -0300

    chore: create user model

---
+++ model/UserModel.cs
@@ -0,0 +1,19 @@
+class User
+{
+    private int id;
+    private String name;
+    private String note;
+
+    public int Id
+    {
+        get { return id; } set { id = value; }
+     }
+    public String Name
+    {
+        get { return name; } set { name = value; }
+    }
+    public String Note
+    {
+        get { return note; } set { name = value; }
+    }
+}
